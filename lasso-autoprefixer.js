7f47202b5d6ad3680a0b723f452e2b0d4304b55c
spadmanabhan <spadmanabhan@ebay.com>
2015-08-25 11:12:29 -0700

    adding default config option

--- lasso-autoprefixer.js
+++ lasso-autoprefixer.js
@@ -1,15 +1,23 @@
+'use strict';
+
 var autoprefixer = require('autoprefixer-core');
 var postcss = require('postcss');
+var DEFAULT_CONFIG = {
+    browsers: ['last 2 versions']
+};
 
-module.exports = function (lasso, config) {
+module.exports = function(lasso, config) {
+    // use DEFAULT_CONFIG if config not present
+    config = config || DEFAULT_CONFIG;
+
     lasso.addTransform({
         contentType: 'css',
         name: module.id,
         stream: false,
-        transform: function (code, lassoContext) {
-            var processed = postcss([ autoprefixer(config) ]).process(code);
+        transform: function(code) {
+            var processed = postcss([autoprefixer(config)]).process(code);
 
-            processed.warnings().forEach(function (warn) {
+            processed.warnings().forEach(function(warn) {
                 process.stderr.write(warn.toString());
             });
             return processed.css;
