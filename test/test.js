fc18fd541d27f65d2c9a0c388e459756de0dd349
Austin Kelleher <austin.kell47@gmail.com>
2018-02-06 13:45:45 -0500

    Add a basic test for validating autoprefix functionality.

---
+++ test/test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var chai = require('chai');
+chai.Assertion.includeStack = true;
+require('chai').should();
+var expect = require('chai').expect;
+var nodePath = require('path');
+
+var lassoAutoprefixer = require('../'); // Load this module just to make sure it works
+var lasso = require('lasso');
+
+describe('lasso-autoprefixer' , function() {
+    it('should prefix a css file', function (done) {
+        var myLasso = lasso.create({
+            fileWriter: {
+                fingerprintsEnabled: true,
+                outputDir: nodePath.join(__dirname, 'static')
+            },
+
+            plugins: [
+                {
+                    plugin: lassoAutoprefixer,
+                }
+            ]
+        });
+
+        myLasso.lassoPage({
+            pageName: 'test',
+            dependencies: [
+                nodePath.join(__dirname, './fixtures/test.css')
+            ]
+        }, function (err, page) {
+            if (err) return done(err);
+            expect(page.getCSSUrls()).to.deep.equal(['/static/test-a2c9d362.css'])
+            done();
+        });
+    });
+});
